b5d51193614b000a97cfc77bdaa07cf2f6fffaea
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-18 00:16:52 +0300

    feat: add createElement block

--- todoList/js/script.js
+++ todoList/js/script.js
@@ -1,7 +1,8 @@
 `use strict`
 const mainBtn = document.querySelector('.todo-btn')
-const todoText = document.querySelector('.todo-text')
 const input = document.querySelector('.todo-input')
+const todoList = document.querySelector('.todo-list')
+
 
 let sum = 0
 mainBtn.addEventListener('click',hey)
@@ -9,7 +10,29 @@ mainBtn.addEventListener('click',hey)
 function hey(e) {
     
     e.preventDefault()
-
+    
+    const todoBlock = document.createElement('div')
+    todoBlock.classList.add('todo')
+    
+    const todoText = document.createElement('p')
+    todoText.classList.add('todo-text')
+    todoBlock.appendChild(todoText)
     todoText.textContent = input.value
-    input.value=''
+
+    const checkBtn = document.createElement('button')
+    checkBtn.classList.add('check-btn')
+    todoBlock.appendChild(checkBtn)
+    console.log(checkBtn)
+    
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete-btn')
+    todoBlock.appendChild(deleteBtn)
+    console.log(deleteBtn)
+    
+
+    todoList.appendChild(todoBlock)
+    
 }
+/* console.log(todoBlock)
+todoBlock.textContent = input.value
+input.value='' */
