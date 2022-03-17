b3bf8ed28657eb55e0c66e68c5ba9739a263cfd8
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-17 23:43:17 +0300

    feat: add firstly code

--- todoList/js/script.js
+++ todoList/js/script.js
@@ -1,1 +1,15 @@
 `use strict`
+const mainBtn = document.querySelector('.todo-btn')
+const todoText = document.querySelector('.todo-text')
+const input = document.querySelector('.todo-input')
+
+let sum = 0
+mainBtn.addEventListener('click',hey)
+
+function hey(e) {
+    
+    e.preventDefault()
+
+    todoText.textContent = input.value
+    input.value=''
+}
