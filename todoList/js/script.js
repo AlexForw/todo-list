1f49b33984030a86ccc4abd1730a93fa75d41546
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-19 01:34:34 +0300

    feat: add main code, bunch with local storage

--- todoList/js/script.js
+++ todoList/js/script.js
@@ -2,7 +2,7 @@
 const mainBtn = document.querySelector('.todo-btn')
 const input = document.querySelector('.todo-input')
 const todoList = document.querySelector('.todo-list')
-
+document.addEventListener('DOMContentLoaded', buildTodo)
 
 let sum = 0
 mainBtn.addEventListener('click',hey)
@@ -23,6 +23,8 @@ function hey(e) {
     todoText.classList.add('todo-text')
     todoBlock.appendChild(todoText)
     todoText.textContent = input.value
+
+    storageInput(input.value)
 
     const checkBtn = document.createElement('button')
     checkBtn.classList.add('check-btn')
@@ -49,8 +51,90 @@ function hey(e) {
     })
 
     deleteBtn.addEventListener('click',function(){
+        todoBlock.classList.add('delete-anim')
         todoList.removeChild(todoBlock)
-    })
+        
+        let todo = JSON.parse(localStorage.getItem('todo'))
+        let index = todo.indexOf(input.value)
+        todo.splice(todo[index], 1)
+        // todo = []
+        localStorage.setItem('todo', JSON.stringify(todo))
+        // setInterval(del, 500)
+        // removeTodo()
+    },once=true)
 
-    
+    function del() {
+        todoList.removeChild(todoBlock)
+    }
 }
+
+
+function storageInput(value) {
+    let todo
+    if(localStorage.getItem('todo') === null) {
+        todo = []
+    }else{
+        todo = JSON.parse(localStorage.getItem('todo'))
+    }
+    todo.push(value)
+    // todo = []
+    localStorage.setItem('todo', JSON.stringify(todo))
+}
+
+function buildTodo() {
+    let todo
+    if(localStorage.getItem('todo') === null) {
+        todo = []
+    }else{
+        todo = JSON.parse(localStorage.getItem('todo'))
+    }
+    todo.forEach(function(todos){
+
+        const todoBlock = document.createElement('div')
+        todoBlock.classList.add('todo')
+        
+        const todoText = document.createElement('p')
+        todoText.classList.add('todo-text')
+        todoBlock.appendChild(todoText)
+        todoText.textContent = todos
+    
+    
+        const checkBtn = document.createElement('button')
+        checkBtn.classList.add('check-btn')
+        checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+        todoBlock.appendChild(checkBtn)
+        
+        const deleteBtn = document.createElement('button')
+        deleteBtn.classList.add('delete-btn')
+        deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+        todoBlock.appendChild(deleteBtn)
+     
+        
+        todoList.insertBefore(todoBlock, todoList.firstChild);
+
+
+        checkBtn.addEventListener('click', function(){
+            todoBlock.classList.toggle('check')
+            if(todoBlock.classList.contains('check')){
+                todoList.appendChild(todoBlock)
+            }
+        })
+    
+        deleteBtn.addEventListener('click',function(){
+            todoBlock.classList.add('delete-anim')
+            todoList.removeChild(todoBlock)
+            
+            let todo = JSON.parse(localStorage.getItem('todo'))
+            let index = todo.indexOf(todos)
+            todo.splice(todo[index], 1)
+            // todo = []
+            localStorage.setItem('todo', JSON.stringify(todo))
+            // setInterval(del, 500)
+            // removeTodo()
+        },once=true)
+        
+        
+        
+    })
+}
+
